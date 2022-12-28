daf2f522317f8f040ba7bd462db030fec0c4ba61
ds <2601948@gmail.com>
2022-12-28 11:14:56 +0900

    잔신잔왕

--- common/utils/src/main/java/com/soon83/LinkUtil.java
+++ common/utils/src/main/java/com/soon83/LinkUtil.java
@@ -4,23 +4,21 @@ import lombok.SneakyThrows;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 
 public class LinkUtil {
 
-    @SneakyThrows
     public static URI location(Long id) {
         String path = (id == null) ? "" : "/" + id;
         return getUri(path);
     }
 
-    @SneakyThrows
     public static URI location(String token) {
         String path = (token == null) ? "" : "/" + token;
         return getUri(path);
     }
 
-    private static URI getUri(String path) throws URISyntaxException {
+    @SneakyThrows
+    private static URI getUri(String path) {
         String requestUri = ServletUriComponentsBuilder.fromCurrentRequest().toUriString();
         return new URI(requestUri.substring(requestUri.indexOf("/", 8)) + path);
     }
